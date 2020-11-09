6a7559b9632075f792ec59ec8fdef42758783c90
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-11-08 20:09:13 -0500

    staarted

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component, useState, useEffect } from "react";
 import "./App.css";
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>SMURFS! W/Redux</h1>
-//         <div>Welcome to your state management version of Smurfs!</div>
-//         <div>Start inside of your `src/index.js` file!</div>
-//         <div>Have fun!</div>
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>SMURFS! W/Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        <div>Start inside of your `src/index.js` file!</div>
+        <div>Have fun!</div>
+      </div>
+    );
+  }
+}
 import axios from "axios";
 import Smurfs from "./Smurfs";
 import addSmurfs from "./contexts/addSmurfs";
